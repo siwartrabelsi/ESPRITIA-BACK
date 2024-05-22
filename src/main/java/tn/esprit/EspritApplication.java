b4f529611314a2189b69b6aeeeca2824ee727d3f
Siwar <siwar.trabelsi@esprit.tn>
2024-05-22 21:55:26 +0100

    fc

---
+++ src/main/java/tn/esprit/EspritApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspritApplication.class, args);
+	}
+
+}
