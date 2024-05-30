46bba2a725a374fd574757f69d164ebebfcf2353
Siwar <siwar.trabelsi@esprit.tn>
2024-05-30 15:25:15 +0100

    master

---
+++ src/main/java/tn/esprit/entities/User.java
@@ -0,0 +1,30 @@
+package tn.esprit.entities;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+	/**
+	* 
+	*/
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+}
